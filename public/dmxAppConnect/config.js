49e17a06a3c5a86a520d4f8e1eb2e4e4dcdb8be2
Heiko Kanzler <heiko@paperclipstudios.com>
2023-11-24 11:48:03 +0100

    Added user data entry form and SQLite database

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,38 @@
+dmx.config({
+  "index": {
+    "iterator1": {
+      "meta": [
+        {
+          "type": "number",
+          "name": "id"
+        },
+        {
+          "type": "text",
+          "name": "name"
+        },
+        {
+          "type": "text",
+          "name": "email"
+        }
+      ],
+      "outputType": "array"
+    },
+    "iterator2": {
+      "meta": [
+        {
+          "type": "number",
+          "name": "id"
+        },
+        {
+          "type": "text",
+          "name": "name"
+        },
+        {
+          "type": "text",
+          "name": "email"
+        }
+      ],
+      "outputType": "array"
+    }
+  }
+});
